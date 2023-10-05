f745ea2630caa5de83b0dae895361f5ba9ab92d8
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-05 13:18:29 +0300

    Initial Commit

---
+++ NextButton/NextButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextButtonApp.swift
+//  NextButton
+//
+//  Created by Николай Игнатов on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NextButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
